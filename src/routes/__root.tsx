74f01f78748a15949c9138d86b4e1697c998fa67
vitinzins <jjeletronicos86@gmail.com>
2026-08-26 09:00:10 -0300

    Add files via upload

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -103,8 +103,14 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         href: appCss,
       },
       { rel: "icon", href: "/favicon.png", type: "image/png" },
+      { rel: "preconnect", href: "https://www.googletagmanager.com" },
     ],
     scripts: [
+      {
+        type: "text/javascript",
+        children:
+          "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PQZ6BFDP');",
+      },
       {
         type: "application/ld+json",
         children: JSON.stringify({
@@ -142,6 +148,14 @@ function RootShell({ children }: { children: ReactNode }) {
         <HeadContent />
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PQZ6BFDP"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
         <Scripts />
       </body>
